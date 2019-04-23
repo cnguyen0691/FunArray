dd77dbd0fea005c4eec0a6c479e6433b6893f107
cnguyen0691 <chaunguyen0691@gmail.com>
2019-04-23 16:54:06 -0400

    adding

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,28 +17,45 @@ public class Main {
         colors.add ("White");
         colors.add ("Black");
         colors.add ("Yellow");
-
-       System.out.println("Colors: " + colors);
-
+      /*  for (String color:colors){
+            System.out.println("Colors: " + color);
+        }
+*/
        //Using the array list created above, print out the value at index 1.
        System.out.println("Colors: " + colors.get(1));
 
        //Using the array list above, prompt the user for one new color,
         // and add that color to the array list.
         Scanner color = new Scanner(System.in);
-        System.out.println("Add color: ");
+
+       /* System.out.println("Add color: ");
         String newColor = color.nextLine();
         colors.add(newColor);
-        System.out.println("Colors: "+colors);
+        System.out.println("Colors: "+colors);*/
 
 
         //Using the array list, continue prompting the user for colors until they type in "Q" or "q" to quit.
         // Each time the user enters a color, add that to the array list. Print out the list at the end.
-       
+        String ans = "";
 
+        while(!ans.equalsIgnoreCase("Q")){
+            System.out.println("Add color: ");
+            String newColor = color.nextLine();
+            colors.add(newColor);
 
+            for (String name:colors){
+                System.out.println("Colors: " + name);
+            }
+
+            System.out.println("Add color: ");
+            ans = color.nextLine();
+        }
+        for (String name:colors){
+            System.out.println("Colors: " + name);
+        }
+        }
     }
-  }
+
 
 
 
